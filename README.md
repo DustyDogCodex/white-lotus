90be9898d0bcc3a074e837741d0a4b440f224210
vmalaviya <varunmalaviya7@gmail.com>
2023-10-19 16:04:46 -0400

    redoing amenities section to improve aesthetics

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # White Lotus
+
+Image Credits:
+- Photo by <a href="https://unsplash.com/@gaddafirusli?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Gaddafi Rusli</a> on <a href="https://unsplash.com/photos/aerial-view-photography-of-boats-on-seashore-2ueUnL4CkV8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+- Photo by <a href="https://unsplash.com/@marcbabin?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Marc Babin</a> on <a href="https://unsplash.com/photos/landscape-photography-of-two-pergolas-near-body-of-water-9IzrXQakxk0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- Photo by <a href="https://unsplash.com/@akaunas?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Sasha Kaunas</a> on <a href="https://unsplash.com/photos/brown-wooden-table-and-chairs-on-brown-wooden-deck-near-body-of-water-during-daytime-TAgGZWz6Qg8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- Photo by <a href="https://unsplash.com/@moseshalym?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Moosa Haleem</a> on <a href="https://unsplash.com/photos/green-palm-tree-on-white-sand-beach-during-daytime-758Km8MAcyI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+- Photo by <a href="https://unsplash.com/@marcbabin?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Marc Babin</a> on <a href="https://unsplash.com/photos/pineapple-juice-on-white-wooden-surface-22fsBrruHPQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
--- src/components/Amenities.tsx
+++ src/components/Amenities.tsx
@@ -1,15 +1,17 @@
-import { motion, useTransform, useScroll } from "framer-motion"
-import { amenities } from "../data"
+import { motion } from "framer-motion"
+import beachHuts from '../assets/beach-huts.jpg'
+import boats from '../assets/boats.jpg'
+import pineappleDrink from '../assets/pineapple-drink.jpg'
+import resort from "../assets/resort.jpg"
+import resortPool from "../assets/resort-pool.jpg"
 
 function Amenities() {
-    const { scrollYProgress } = useScroll()
-    const scale = useTransform(scrollYProgress, [0, 0.6], [0.5, 1])
-
     return (
         <section
             id="amenities"
             className="mt-10 py-10 bg-neutral-200/60 flex flex-col items-center justify-center" 
         >
+            {/* animated section heading */}
             <motion.div
                 initial="hidden"
                 whileInView="visible"
@@ -23,93 +25,26 @@ function Amenities() {
                 <h1
                     className="mt-10 text-3xl mb-6 text-center font-thin"
                 >
-                    AMENITIES
+                    Why You Should Visit Us
                 </h1>
             </motion.div>
+            
+            {/* MAIN CONTENT */}
+            <div>
+                {/* left side */}
+                <div>   
+                    <img src={beachHuts} alt="Resort huts on the beach" />
+                    <img src={boats} alt="Boats on the beach" />
+                    <img src={pineappleDrink} alt="Pineappe Mojito" />
+                    <img src={resort} alt="Resort" />
+                    <img src={resortPool} alt="Pool at the resort" />
+                </div>
 
-            <motion.div
-                style={{ scale }}
-                className="flex items-center justify-between lg:w-3/5"
-            >
-                <motion.div
-                    style={{
-                        scaleY: scrollYProgress
-                    }}
-                    className="flex flex-col md:flex-row items-center"
-                >
-                    <img 
-                        src={amenities[0].image} 
-                        alt="" 
-                        className="rounded-lg h-80 md:h-full w-full md:w-1/2"
-                    />
-                    <div
-                        className="p-10"
-                    >
-                        <h3
-                            className="text-2xl font-bold mb-6 text-center"
-                        >
-                            {amenities[0].title}
-                        </h3>
-                        <p className="text-center text-lg mt-3">{amenities[0].summary}</p>
-                    </div>
-                </motion.div>
-            </motion.div>
+                {/* right side */}
+                <div>
 
-            <motion.div
-                style={{ scale }}
-                className="flex items-center justify-between lg:w-3/5"
-            >
-                <motion.div
-                    style={{
-                        scaleY: scrollYProgress
-                    }}
-                    className="flex flex-col md:flex-row-reverse items-center"
-                >
-                    <img 
-                        src={amenities[1].image} 
-                        alt="" 
-                        className="rounded-lg h-80 md:h-full w-full md:w-1/2"
-                    />
-                    <div
-                        className="p-10"
-                    >
-                        <h3
-                            className="text-2xl font-bold mb-6 text-center"
-                        >
-                            {amenities[1].title}
-                        </h3>
-                        <p className="text-center text-lg mt-3">{amenities[1].summary}</p>
-                    </div>
-                </motion.div>
-            </motion.div>
-
-            <motion.div
-                style={{ scale }}
-                className="flex items-center justify-between lg:w-3/5"
-            >
-                <motion.div
-                    style={{
-                        scaleY: scrollYProgress
-                    }}
-                    className="flex flex-col md:flex-row items-center"
-                >
-                    <img 
-                        src={amenities[2].image} 
-                        alt="" 
-                        className="rounded-lg h-80 md:h-full w-full md:w-1/2"
-                    />
-                    <div
-                        className="p-10"
-                    >
-                        <h3
-                            className="text-2xl font-bold mb-6 text-center"
-                        >
-                            {amenities[2].title}
-                        </h3>
-                        <p className="text-center text-lg mt-3">{amenities[2].summary}</p>
-                    </div>
-                </motion.div>
-            </motion.div>
+                </div>
+            </div>
         </section>
     )
 }
