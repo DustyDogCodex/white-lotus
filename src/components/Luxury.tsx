4f891abf29008787db6ac5dc5d16e6606d1ea9ee
vmalaviya <varunmalaviya7@gmail.com>
2023-10-19 18:00:12 -0400

    setup a grid layout for images in the new Luxury section

--- src/components/Luxury.tsx
+++ src/components/Luxury.tsx
@@ -4,19 +4,19 @@ import Luxury3 from "../assets/Luxury3.jpg"
 
 function Luxury() {
     return (
-        <div className="w-full grid h-screen bg-sky-200 px-4">
-            <div className="h-[600px]">
+        <div className="w-full flex justify-center h-screen px-4 pt-16 md:mb-[30%]">
+            <div className="h-[600px] grid lg:grid-cols-3 gap-5 w-full lg:w-4/5 bg-sky-200 p-8">
                 {/* intro */}
-                <div className="relative lg:top-20 col-span-1 lg:col-span-2 py-12 w-full lg:w-4/5">
-                    <h3 className="text-2xl font-bold">Luxury Beyond Measure</h3>
-                    <p className="pt-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores architecto facere distinctio vero, laboriosam tempore animi odit molestias, harum placeat perferendis libero cumque? Harum voluptatum perferendis ea ex aliquid corporis?</p>
+                <div className="relative lg:top-20 col-span-2 lg:col-span-1 py-12">
+                    <h3 className="text-4xl font-bold">Luxury Beyond Measure</h3>
+                    <p className="pt-5 text-base md:text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores architecto facere distinctio vero, laboriosam tempore animi odit molestias, harum placeat perferendis libero cumque? Harum voluptatum perferendis ea ex aliquid corporis?</p>
                 </div>
 
                 {/* images */}
                 <div className="grid grid-cols-2 col-span-2 gap-3">
-                    <img src={Luxury1} alt="Luxurious amenities" />
-                    <img src={Luxury2} alt="Luxurious amenities" />
-                    <img src={Luxury3} alt="Luxurious amenities" />
+                    <img className="object-cover w-full h-full" src={Luxury1} alt="Luxurious amenities" />
+                    <img className="row-span-2 object-cover w-full h-full" src={Luxury2} alt="Luxurious amenities" />
+                    <img className="object-cover w-full h-full" src={Luxury3} alt="Luxurious amenities" />
                 </div>
             </div>
         </div>
