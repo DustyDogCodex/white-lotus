93856a3a057c221f19e91d473c4ceb1c09d93eae
vmalaviya <varunmalaviya7@gmail.com>
2023-10-20 16:13:28 -0400

    improved image slider in Media.tsx

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -3,97 +3,107 @@ import { AnimatePresence, motion } from "framer-motion"
 import { useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import { wrap } from "framer-motion"
+
+//variants object for animation
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0
+    }
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0
+    }
+  }
+}
+
+/* The less distance a user has swiped, the more velocity they need to register as a swipe */
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity
+}
 
 function Media() {
-    const [ position, setPosition ] = useState<number>(0)
-    const [ direction, setDirection ] = useState<string>('')
-    
-    const nextImage = () => {
-        setDirection('right')
-        if(position === mediaImages.length - 1){
-            setPosition(0)
-        } else {
-            setPosition(position + 1)
-        }
-    }
+    const [[ page, direction ], setPage ] = useState([0, 0])
 
-    const prevImage = () => {
-        setDirection('left')
-        if(position === 0){
-            setPosition(mediaImages.length - 1)
-        } else {
-            setPosition(position - 1)
-        }
-    }
+    // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
+    // then wrap that within 0-2 to find our image ID in the array below. By passing an
+    // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
+    // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
+    const imageIndex = wrap(0, mediaImages.length, page)
 
-    //variants object for motion.img
-    const variants = {
-        initial:{  
-            opacity: 0, 
-            x: direction === 'right' ? 100 : -100 
-        },
-        animate: { 
-            opacity: 1, 
-            x: 0,
-            transition: {
-                x: { 
-                    type: 'spring',
-                    stiffness: 300,
-                    damping: 30
-                },
-                opacity: {
-                    duration: 0.2
-                }
-            }
-        },
-        exit:{ 
-            opacity: 0, 
-            x: direction === 'left' ? -100 : 100 
-        }
+    const paginate = (newDirection: number) => {
+        setPage([page + newDirection, newDirection])
     }
 
     return (
         <section
             id="gallery"
-            className="w-full h-full flex flex-col justify-center items-center bg-black py-10 px-3"
+            className="w-full h-full flex justify-center items-center bg-black py-10 px-3"
         >
-            <h2 className="text-3xl text-white mb-5">Gallery</h2>
             <div
                 className="relative h-[300px] md:h-[800px] w-[350px] md:w-full overflow-hidden lg:w-3/5" 
             >
+                <h2 className="text-3xl text-white text-center mb-5">Gallery</h2>
                 <AnimatePresence initial={false} custom={direction}>
                     <motion.img 
-                        key={mediaImages[position]}
-                        src={mediaImages[position]} 
-                        alt="images" 
-                        className="rounded-lg h-full w-full object-cover" 
+                        className="rounded-lg object-cover absolute w-full h-full" 
+                        alt="Image slider" 
+                        key={page}
+                        src={mediaImages[imageIndex]} 
                         variants={variants}  
-                        initial="initial"
-                        animate='animate'
+                        initial="enter"
+                        animate="center"
                         exit='exit'
                         custom={direction}
+                        transition={{
+                            x: { type: "spring", stiffness: 300, damping: 30 },
+                            opacity: { duration: 0.2 }
+                        }}
+                        drag="x"
+                        dragConstraints={{ left: 0, right: 0 }}
+                        dragElastic={1}
+                        onDragEnd={(e, { offset, velocity }) => {
+                            const swipe = swipePower(offset.x, velocity.x);
+
+                            if (swipe < -swipeConfidenceThreshold) {
+                                paginate(1)
+                            } else if (swipe > swipeConfidenceThreshold) {
+                                paginate(-1)
+                            }
+                        }}
                     />
                 </AnimatePresence>
 
                 {/* button to go left */}
                 <div
-                    className="absolute top-[45%] left-3 cursor-pointer p-2 flex items-center rounded-full bg-black/70 transition duration-300 hover:bg-indigo-600"
+                    className="absolute top-[50%] z-20 left-3 cursor-pointer p-2 flex items-center rounded-full bg-black/70 hover:bg-indigo-600 transition duration-300"
                 >
                     <FontAwesomeIcon 
                         icon={faChevronLeft} 
                         style={{color: "white", height:'30px', width:'30px'}} 
-                        onClick={prevImage}
+                        onClick={() => paginate(1)}
                     />
                 </div> 
 
                 {/* button to go right */} 
                 <div
-                    className="absolute top-[45%] right-3 cursor-pointer p-2 flex items-center rounded-full bg-black/70 transition duration-300 hover:bg-indigo-600"
+                    className="absolute top-[50%] z-20 right-3 cursor-pointer p-2 flex items-center rounded-full bg-black/70 hover:bg-indigo-600 transition duration-300"
                 >
                     <FontAwesomeIcon 
                         icon={faChevronRight} 
                         style={{color: "white", height:'30px', width:'30px'}}
-                        onClick={nextImage}
+                        onClick={() => paginate(-1)}
                     />
                 </div>  
             </div>
