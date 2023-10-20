ded9b04fe7231392681fd8b24ddc1f19f8d1f84d
vmalaviya <varunmalaviya7@gmail.com>
2023-10-20 17:20:11 -0400

    getting project ready for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    /* comment or remove this to run the app on netlify. Uncomment to run on github pages.  */
+    base:'/white-lotus/'
 })
